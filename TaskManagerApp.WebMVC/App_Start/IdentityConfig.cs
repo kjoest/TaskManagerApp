129339e377a0308b10c52a0dcfa43897b8ed6a13
kyle Joest <kjoest92@gmail.com>
2023-11-09 11:41:51 -0600

    Brought in all references

--- TaskManagerApp.WebMVC/App_Start/IdentityConfig.cs
+++ TaskManagerApp.WebMVC/App_Start/IdentityConfig.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using TaskManagerApp.Data;
 using TaskManagerApp.WebMVC.Models;
 
 namespace TaskManagerApp.WebMVC
--- TaskManagerApp.WebMVC/Controllers/AccountController.cs
+++ TaskManagerApp.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using TaskManagerApp.Data;
 using TaskManagerApp.WebMVC.Models;
 
 namespace TaskManagerApp.WebMVC.Controllers
